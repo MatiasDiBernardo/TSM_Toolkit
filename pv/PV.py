cb460f2219bee5b1f9cbbf86bdb3fbec03dc710c
Matías Di Bernardo <matias.di.bernardo@hotmail.com>
2023-05-28 20:11:05 -0300

    PV first test

--- pv/PV.py
+++ pv/PV.py
@@ -1,32 +1,116 @@
 import numpy as np
-import librosa
+from utils.wav_utils import read_wav, save_wav
+from scipy.fftpack import fft, ifft
+from scipy.signal import get_window
 
-#Ver tema factor N y arregar el tema de importar cosas de otras folders.
+def normalize_phase(phi_diffrenece):
+    """Normalice the value of difference between
+    the real and the predicted phase.
+
+    Args:
+        phi_diffrenece (float): Phase difference
+
+    """
+    
+    print("El valor de correctión de fase es: ", phi_diffrenece)
+    a = 0
+    normalize_phase = np.zeros(len(phi_diffrenece))
+    for i, phi in enumerate(phi_diffrenece):
+        if phi > 0.5:
+            normalize_phase[i] = 0.5
+            continue
+        if phi < -0.5:
+            normalize_phase[i] = -0.5
+            continue
+        normalize_phase[i] = phi
+    
+    return normalize_phase
+    
+
+def instantanius_frequency(Xk, phi_pred, m, fs, Ha):
+    """Calculates the instantanius frequency of a current frequency frame. 
+    And also predicts the phase for the next frame.
+
+    Args:
+        Xk (np.array): Frequency spectrum in one frame time.
+        phi_pred (float): Predicted phase of the current frame.
+        m (int): Frame index.
+        Fs (int): Sample rate.
+        Ha (int): Analisis hop size.
+    """
+    #Calculate necessary parameters
+    N = len(Xk)
+    t1 = (m*Ha)/fs
+    t2 = ((m + 1)*Ha)/fs
+    delta_t = t2 - t1  #Same as Ha/fs
+    k = np.arange(0,N)
+    w = k * fs/N
+
+    #Calculate current and next phase
+    phi_real = np.angle(Xk)
+    phi_pred_next_frame = phi_real + w*delta_t
+
+    #Adjust frequencies on the current frame
+    phi_offset = normalize_phase(phi_real - phi_pred)
+    IF_w = w + (phi_offset/delta_t)
+    print("Comparo w y su clon: ", w, IF_w)
+
+    return IF_w, phi_pred_next_frame
 
 def phase_vocoder(x, fs, N, alpha, Hs):
 
     if alpha == 1:
         return x
     if alpha < 1:
-        y = np.zeros(int(len(x) * alpha) + N)  #N es factor de compensasión pero se puede sacar
+        y = np.zeros(int(len(x) * alpha) + N)  #N accounts for last frame.
     if alpha > 1:
-        y = np.zeros(int(len(x) * alpha))  #N es factor de compensasión pero se puede sacar
+        y = np.zeros(int(len(x) * alpha)) 
     
     Ha = int(Hs/alpha)
+    win_type = "hann"
     
-    cicles = int((len(x) - N)/Ha) + 1
-    
-    for i in range(cicles):
-        Xm = x[i * Ha: N + i * Ha]
-        y[i * Hs: N + i * Hs] += Xm
+    cicles = int((len(x) - N)/Ha) + 1  #Amount of frames in the signal.
+    pred_phase = 0
+    mod_phase = 0
+
+    for m in range(cicles):
+        #Segment frame and transform it to frequency.
+        Xm = x[m * Ha: N + m * Ha]
+        w = get_window("hann", N)
+        Xk = fft(Xm * w)
+
+        #Modify frequency frame by shifting the phase.
+        IF_w, next_phase_pred = instantanius_frequency(Xk, pred_phase, m, fs, Ha)
+        X_mod = np.abs(Xk) * np.exp(1j * 2*np.pi * mod_phase)
+
+        #Resets values for next iteration
+        pred_phase = next_phase_pred
+        mod_phase = mod_phase + IF_w * Ha/fs
+
+        #Transform to time and relocate in the synthesis frame.
+        Xm_mod = ifft(X_mod)
+        Xm_mod = np.real(Xm_mod)
+        y[m * Hs: N + m * Hs] += Xm_mod
         
     return y
 
-from utils.wav_utils import read_wav, save_wav
+def quick_test(path, N, alpha, Hs):
+    
+    fs = 22050
+    x, _ = read_wav(path, fs)
 
-fs = 22050
-x, _ = read_wav("data\piano_cerca.wav", fs)
+    rta = phase_vocoder(x, fs, N, alpha, Hs)
 
-rta = phase_vocoder(x, fs, 2048, 0.8, 512)
+    save_wav(rta, fs, "data\\quick_test.wav")
 
-save_wav(rta, fs, "data\\test_ola.wav")
+"""
+Si uso fs igual 22050 y una ventana de 2048 tengo una longitud de
+93ms. 
+"""
+
+N = 2048
+Hs = N//4
+alpha = 0.8
+
+quick_test("data\\piano_cerca.wav", N, alpha, Hs)
+
