3f00f3131252015ff8ab07905d2523fcb7066eb5
Sebas Carro <carro57195@estudiantes.untref.edu.ar>
2023-06-07 17:24:23 -0300

    add libs

--- ola.py
+++ ola.py
@@ -1,5 +1,8 @@
-#Acá va la implementación del OLA
+#Implementacion OLA 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import get_window
+from utils.wav_utils import read_wav, save_wav
+from utils.windows import window_normalization
 
-#prueba 
 
-a=1
