ade2203225d2f5edfd1117b8ac9a2a4e2143e061
Sebas Carro <carro57195@estudiantes.untref.edu.ar>
2023-06-07 17:40:50 -0300

    agrego basics

--- ola.py
+++ ola.py
@@ -5,4 +5,40 @@ from scipy.signal import get_window
 from utils.wav_utils import read_wav, save_wav
 from utils.windows import window_normalization
 
+def TSM_OLA(x, fs, N, alpha, Hs):
+    """Applies TSM procedure based on OLA: Overlapp and Add.
 
+    Args:
+        x (np.array): Audio signal.
+        fs (int): Sample rate.
+        N (int): Window lenght.
+        alpha (float): Stretching factor.
+        Hs (int): Synthesis hopsize length.
+
+    Returns:
+        np.array: Time modify audio signal.
+    """
+
+    if alpha == 1:
+        return x
+    if alpha < 1:
+        y = np.zeros(int(len(x) * alpha) + N)  #N accounts for last frame.
+    if alpha > 1:
+        y = np.zeros(int(len(x) * alpha)) 
+    
+    #Window election
+    win_type = "hann"
+    w = get_window(win_type, N)
+    w_norm = window_normalization(w, Hs)
+
+    #Inicialization
+    Ha = int(Hs/alpha)
+    cicles = int((len(x) - N)/Ha) + 1  #Amount of frames in the signal.
+
+    for m in range(cicles):
+
+        #compute analysis frames
+        x_m = x[m * Ha: N + (m * Ha)]
+       
+        
+    return 
