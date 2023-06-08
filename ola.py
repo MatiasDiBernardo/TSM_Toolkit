c7172d9ef809955d95bd42ac2cdd618eb9f54977
Sebas Carro <carro57195@estudiantes.untref.edu.ar>
2023-06-08 20:23:09 -0300

    agrego carpeta audios

--- ola.py
+++ ola.py
@@ -5,12 +5,11 @@ from scipy.signal import get_window
 from utils.wav_utils import read_wav, save_wav
 from utils.windows import window_normalization
 
-def TSM_OLA(x, fs, N, alpha, Hs):
+def TSM_OLA(x, N, alpha, Hs): 
     """Applies TSM procedure based on OLA: Overlapp and Add.
 
     Args:
         x (np.array): Audio signal.
-        fs (int): Sample rate.
         N (int): Window lenght.
         alpha (float): Stretching factor.
         Hs (int): Synthesis hopsize length.
@@ -19,9 +18,10 @@ def TSM_OLA(x, fs, N, alpha, Hs):
         np.array: Time modify audio signal.
     """
 
+    #defining output vector size according to scale factor
     if alpha == 1:
-        return x
-    if alpha < 1:
+        return x 
+    if alpha < 1: 
         y = np.zeros(int(len(x) * alpha) + N)  #N accounts for last frame.
     if alpha > 1:
         y = np.zeros(int(len(x) * alpha)) 
@@ -29,7 +29,7 @@ def TSM_OLA(x, fs, N, alpha, Hs):
     #Window election
     win_type = "hann"
     w = get_window(win_type, N)
-    w_norm = window_normalization(w, Hs)
+    w_norm = window_normalization(w, Hs) 
 
     #Inicialization
     Ha = int(Hs/alpha)
@@ -37,8 +37,47 @@ def TSM_OLA(x, fs, N, alpha, Hs):
 
     for m in range(cicles):
 
-        #compute analysis frames
+        #segment input into analysis frames 
         x_m = x[m * Ha: N + (m * Ha)]
-       
-        
-    return 
+
+        #compute output signal by windowing x_m and normalizing
+        #and overlapping according to Hs
+        y[m * Hs: N + (m * Hs)] += (x_m*w)/(w_norm) 
+                
+    return y 
+
+
+#funcion de quick test
+def quick_test_OLA(path, N,fs, alpha, Hs,savename):
+    
+    x, _ = read_wav(path, fs)
+    x_out = TSM_OLA(x,N,alpha,Hs)
+    nameout = "audios/"+savename+".wav"
+    save_wav(x_out, fs, nameout)
+
+'''
+Observacion:
+tamaño de ventana en segundos: w_size = N/fs
+
+Para fs = 44100
+
+algunos casos:
+Si N = 1024 -> w_size = 23 ms
+Si N = 2048 -> w_size = 46 ms
+Si N = 4096 -> w_size = 92 ms
+Si N = 8192 -> w_size = 184 ms
+'''
+
+#simple initial tests
+
+'''
+PRUEBA1 - signal: synth - fs=44100 - N=4096 - alpha=1.5 - Hs=N/2
+'''
+quick_test_OLA('audios/synth.wav',4096,44100,1.5,4096//2,'synth_prueba1')
+
+'''
+PRUEBA2 - signal: sharp_bells - fs=44100 - N=4096 - alpha=1.5 - Hs=N/2
+'''
+quick_test_OLA('audios/sharp_bells.wav',4096,44100,1.5,4096//2,'bells_prueba1')
+
+
