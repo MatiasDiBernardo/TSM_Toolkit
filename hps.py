50f985e3139cc4facacb336637155dab2fca5288
matias-veree <matias.veree@hotmail.com>
2023-06-22 15:22:12 -0300

    HPS working

--- hps.py
+++ hps.py
@@ -1,35 +1,12 @@
 
 # %%
-
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fftpack import fft, ifft
 from scipy.signal import stft 
-from scipy.signal import get_window
-from scipy.signal import medfilt2d as median
+from scipy.signal import medfilt as mediann
 from librosa import stft, istft
 
-from utils.wav_utils import read_wav, save_wav
-from utils.windows import window_normalization
-
-#parte de prueba temporal para definir señales
-L = 10000
-fs = 44100
-n = np.arange(0, L-1, 1) 
-f1 = 100
-f2 = 200
-#Componentes armonicas
-x = np.sin(n * (f1/fs) * 2 * np.pi ) + np.sin(n * (f2/fs) * 2 * np.pi )
-#Impulsos
-for i in range(20):# Generamos inpulsos de 20 muestras 
-    x[i+300] = 10
-    x[i+5000] = 10       
-
-plt.plot(n ,x )
-plt.grid()
-plt.show()
-
-def HPS(x, fs, N, M):
+def HPS(x, N, M):
     """ Divides the percusive and harmonics components from the input signal
     returning two diferent signals.
 
@@ -38,7 +15,7 @@ def HPS(x, fs, N, M):
         fs (int): Sample rate.
         N (int): Window lenght for FFT.
         M (int): Median filter window leight, this value must be 
-        odd. In the contrary it will be forced to the upper inmedate near.
+        odd. In the contrary it will be forced to the upper inmedate.
 
     Returns:
         xp (np.array): Percusive signal.
@@ -46,25 +23,19 @@ def HPS(x, fs, N, M):
     """
     #Obtain the STFT and the spectogram Y 
     X = stft(x, n_fft = N )
-    x1 = istft(X,n_fft = N )
-  
     Y = abs(X)
 
-    Mh = median(Y,(1,M)) #2D median filter with window
-    Mp = median(Y,(M,1))   
-    #for in range(len())
-    #if Mh > Mp:
-        
+    Yh = mediann(Y,(1,M)) #2D median filter with window
+    Yp = mediann(Y,(M,1))   
 
-    #Auxiliar testing operation Dspues la borro  
-    Cp = istft(Mp, n_fft = N)
-    Ch = istft(Mh, n_fft = N)
+    Mh = np.zeros(np.shape(Yh))
+    Mp = np.zeros(np.shape(Yh))
 
+    a , b = np.shape(Yh)
     # Evaluamos para cada frecuencia y segmento 
-    a , b = np.shape(Mh)
     for i in range(a):
         for l in range(b):
-            if Mh[i,l] > Mp[i,l]:
+            if Yh[i,l] > Yp[i,l]:
                 Mh[i,l] = 1
             else:
                 Mp[i,l] = 1 
@@ -72,26 +43,55 @@ def HPS(x, fs, N, M):
     Xp = X * Mp
     Xh = X * Mh
 
+    #Inverse transform for each isolated spectrum 
     xp = istft(Xp, n_fft = N)
     xh = istft(Xh, n_fft = N)
 
-    return X, xp, xh, x1
+    #Compensate the gap created by the median filter
+    l = len(x)-len(xp)
+    xp = np.concatenate((xp, np.zeros(l)))
+    xh = np.concatenate((xh, np.zeros(l)))
+  
+    return xp, xh
 
+# %% SEÑAL X 
 
+#parte de prueba temporal para definir señales
+L = 5000
+fs = 44100
+n = np.arange(0, L-1, 1) 
+f1 = 100
+f2 = 400
+#Componentes armonicas
+x = np.sin(n * (f1/fs) * 2 * np.pi ) + np.sin(n * (f2/fs) * 2 * np.pi )
+#Impulsos
+for i in range(20):# Generamos inpulsos de 20 muestras 
+    x[i+300] = 10
+    x[i+3000] = 10       
 
-
-N = 4096
-X , xp, xh, x1 = HPS(x ,fs ,N , 17 )
-y = abs(X)
-
-plt.figure() #Señal original
-plt.specgram(x, NFFT =N, Fs = fs, noverlap = N//4)
-#plt.yscale("log")
-plt.ylim([0, 2000])
-plt.colorbar()
+plt.plot(n ,x )
 plt.grid()
 plt.show()
+# %% USANDO LA FUNCION (BORRAR)
 
+M = 17
+N = 1024
+xp, xh = HPS(x , N , M)
+
+#Waveform armonica  
+n =  np.arange(0,len(xh))
+
+#plt.plot(n, xh)
+
+plt.plot(n, x, 'blue')
+plt.plot(n, xh,'orange')
+plt.axvline(x = M, color = 'red', label = 'axvline - full height')
+plt.show()
+
+plt.plot(n, x, 'blue')
+plt.plot(n, xp,'orange')
+plt.axvline(x = M, color = 'red', label = 'axvline - full height')
+plt.show()
 
 plt.figure() #Espectro mascara percusiva
 plt.specgram(xp, NFFT = N,Fs = fs)
@@ -109,14 +109,6 @@ plt.grid()
 plt.colorbar()
 plt.show()
 
-
-
-#Waveform
-n =  np.arange(0,len(xh))
-plt.plot(n, abs(xh))
-plt.plot(n, abs(xp))
-
-plt.show()
 
 
 # %%
